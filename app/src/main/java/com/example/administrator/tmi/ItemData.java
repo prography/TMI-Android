642080154c367136defe0cd32a9e56a332227937
Lieutenant Kang <blizzard1107@gmail.com>
2018-10-05 01:39:22 +0900

    메인 화면 레이아웃 및 ListView 구현 (View 터치시 새로운 액티비티 연계 생성은 10/7 오전 구현 예정)

---
+++ app/src/main/java/com/example/administrator/tmi/ItemData.java
@@ -0,0 +1,17 @@
+package com.example.administrator.tmi;
+
+public class ItemData {
+
+    public int replyImageId;
+    public int likeImageId;
+    public String nameText;
+    public String dateText;
+    public String subjectText;
+    public String mainText;
+    public String tagsText;
+    public int replyCount;
+
+    public ItemData(){
+
+    }
+}
--- app/src/main/java/com/example/administrator/tmi/MainActivity.java
+++ app/src/main/java/com/example/administrator/tmi/MainActivity.java
@@ -2,12 +2,35 @@ package com.example.administrator.tmi;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    MyAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView)findViewById(R.id.list_view);
+        mAdapter = new MyAdapter(this);
+        listView.setAdapter(mAdapter);
+        initData();
+    }
+
+    private void initData(){
+        for(int i=0;i<20;i++){
+            ItemData d = new ItemData();
+            d.dateText = "10.05";
+            d.nameText = "자색호랑나비";
+            d.likeImageId = R.drawable.heart;
+            d.replyImageId = R.drawable.reply;
+            d.replyCount = 33;
+            d.subjectText = "강남역 인근 주말 주차 장소 추천";
+            d.mainText = "서울이 차 끌고 가기 막막한 곳으로 손꼽히는 강남! 모르면 손해보는 강남역, 신논현역 인근 주말 주차꿀팁";
+            d.tagsText = "#강남 #주말 #주차 꿀팁";
+            mAdapter.add(d);
+        }
+
     }
 }
