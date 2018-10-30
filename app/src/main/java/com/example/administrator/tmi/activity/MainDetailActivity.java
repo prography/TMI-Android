cf433513ba2307412abe635594453b3661667c8f
BBIHONG <dbsghd95@naver.com>
2018-10-30 17:30:02 +0900

    디자인 입히기 작업중05

--- app/src/main/java/com/example/administrator/tmi/activity/MainDetailActivity.java
+++ app/src/main/java/com/example/administrator/tmi/activity/MainDetailActivity.java
@@ -24,11 +24,11 @@ public class MainDetailActivity extends AppCompatActivity {
             }
         });
 
-        TextView detailSubject = (TextView)findViewById(R.id.subject);
-        TextView detailMain = (TextView)findViewById(R.id.main_text);
-        TextView detailName = (TextView)findViewById(R.id.nickname);
-        TextView detailDate = (TextView)findViewById(R.id.date_text);
-        TextView detailTags = (TextView)findViewById(R.id.tags);
+        TextView detailSubject = (TextView)findViewById(R.id.main_detail_subject);
+        TextView detailMain = (TextView)findViewById(R.id.main_detail_text);
+        TextView detailName = (TextView)findViewById(R.id.main_detail_nickname);
+        TextView detailDate = (TextView)findViewById(R.id.main_detail_date);
+        TextView detailTags = (TextView)findViewById(R.id.main_detail_tags);
 
         Intent intent = getIntent();
         detailSubject.setText(intent.getStringExtra("subject"));
